1295b6e0bc949ff5f8b2e0001c34a1117afe0a90
Eduardo Trigo <eduatrigo@gmail.com>
2024-05-25 14:37:08 -0300

    completando tp6 ejercicio 1 y 2

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Invertir Numero!");
+
+int num;
+string cadena = Console.ReadLine();
+
+if (int.TryParse(cadena, out num))
+{
+    if (num > 0)
+    {
+        int aux;
+        int invertido = 0;
+
+        while (num != 0 )
+        {
+            aux = num % 10;
+            num /= 10;
+            invertido = (invertido * 10) + aux;  
+        }
+
+        Console.WriteLine("invertido: " +invertido);
+    }
+}else{
+    Console.WriteLine("Entrada invalida");
+}
